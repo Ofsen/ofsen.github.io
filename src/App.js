b7e63582c7f0b5df825e8891e0e130fb9b45fb04
ofsen <ofsensmailbox@gmail.com>
2020-11-12 22:16:08 +0100

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Hello,</h1>
+				<p>I am currently building this slowly but surely ! </p>
+        <span>i think ...</span>
+        <p>If <a href="https://github.com/Ofsen/ofsen.github.io/tree/release">this repo</a> is updated but the page doesn't change, then i would advice to clear your browser cache.</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
